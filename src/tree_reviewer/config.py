7cd494d5954cff7d2fede495bd2dc3da1e93eceb
Jose Edwards <jflira@miuandes.cl>
2024-08-07 17:53:38 -0400

    add parameters to env

--- src/tree_reviewer/config.py
+++ src/tree_reviewer/config.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv(dotenv_path='./my_parameters.env') # Load environment variables from .env file
+load_dotenv(dotenv_path='./tree_reviewer/my_parameters.env', encoding="utf-8") # Load environment variables from .env file
 
 def get_env(key, default=None):
     value = os.getenv(key)
